b66968327775bc63ca6a545bb264fa3c9dae0b19
huangfy <1457635148@qq.com>
2018-04-28 22:36:23 +0800

    update new

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// console.log(123);
+
+/* eslint-disable no-console,no-alert */
+console.log(123);
+alert(123);
+
+/* eslint-enable */
+console.log(123); // eslint-disable-line
+
+// eslint-disable-next-line
+console.log(123);
